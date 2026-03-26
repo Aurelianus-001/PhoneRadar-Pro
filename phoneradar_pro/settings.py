8c6801e167e2a3def83232ab330386c5630c2c0f
万万刚开始学 <226762210+Aurelianus-001@users.noreply.github.com>
2026-03-26 16:46:15 +0800

    feat: 连接 Neon 云数据库

--- phoneradar_pro/settings.py
+++ phoneradar_pro/settings.py
@@ -76,11 +76,15 @@ WSGI_APPLICATION = 'phoneradar_pro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+import os
+
+# 如果环境变量里有 DATABASE_URL 就连云端，否则连本地 SQLite
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
